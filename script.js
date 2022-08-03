1dcd53c3fda114fafbd8b76d2b1ca3cafe3f055b
Sergey <graygmai1@gmail.com>
2022-08-03 18:08:57 +0500

    upd

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// const ARRAY = [9, 3212555,819,4,5]
-// const ARRAY = [8, 41, 55, 8, 61, 27, 37, 39, 55, 519, 309, 77, 132, 213,]
+// const ARRAY = [9, 3212555, 819, 4, 5]
+// const ARRAY = [8, 41, 55, 8, 61, 27, 37, 39, 55, 519, 309, 77, 132, 213]
 const ARRAY = [41, 55, 61, 1, 8, 27, 37, 39]
 
-let newArr = changeArr(ARRAY)
-console.log('return:', newArr)
+const newArr = changeArr(ARRAY)
+console.log("return:", newArr)
 
 function changeArr(arr) {
-
     const startArrLength = arr.length
 
     filterArr(arr)
@@ -23,19 +22,16 @@ function changeArr(arr) {
 }
 
 function filterArr(arr) {
-
     arr.sort((a, b) => a - b)
 
     for (let i = 0; i < arr.length; i++) {
-
         const deleteMatchArr = []
         const numb = new Set()
 
         const buffNumb = arr[i].toString().split("").map(Number)
-        buffNumb.forEach(item => numb.add(item))
+        buffNumb.forEach((item) => numb.add(item))
 
         for (let j = i + 1; j < arr.length; j++) {
-
             const nextNumb = arr[j].toString().split("").map(Number)
 
             if (findMatch(numb, nextNumb)) {
@@ -48,13 +44,11 @@ function filterArr(arr) {
             arr.splice(i, 1)
             i--
         }
-
     }
     console.log(arr)
 }
 
 function findMatch(numb, nextNumb) {
-
     for (let i = 0; i < nextNumb.length; i++) {
         if (numb.has(nextNumb[i])) return true
     }
@@ -63,19 +57,19 @@ function findMatch(numb, nextNumb) {
 }
 
 function chekToNextStep(prevArrLength, arrLength) {
-
-    if (arrLength < 2) { return true }
-    if (arrLength === prevArrLength) { return true }
+    if (arrLength < 2) {
+        return true
+    }
+    if (arrLength === prevArrLength) {
+        return true
+    }
 
     return false
 }
 
 function reverseAndSquareArr(arr) {
-
     arr.forEach((item, index) => {
-
         const buff = +item.toString().split("").reverse().join("")
         arr[index] = Math.pow(buff, 2)
-
     })
 }
