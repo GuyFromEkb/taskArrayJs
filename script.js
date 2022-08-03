c07c925948d447fd620c28d903cbbeefc16b8405
Sergey <graygmai1@gmail.com>
2022-08-03 06:09:10 +0500

    done

---
+++ script.js
@@ -0,0 +1,84 @@
+// const ARRAY = [9, 3212555]
+// const ARRAY = [41, 55, 61,/*  1, 8, */ 27, 37, 39, 519, 309, 77, 132,]
+const ARRAY = [41, 55, 61, 1, 8, 27, 37, 39]
+
+changeArr(ARRAY)
+
+function changeArr(arr) {
+    const startArrLength = arr.length
+
+    filterArr(arr)
+
+    if (chekToNextStep(startArrLength, arr.length)) return arr
+
+    reverseAndSquareArr(arr)
+
+    changeArr(arr)
+}
+
+
+function filterArr(arr) {
+    arr.sort((a, b) => a - b)
+
+    for (let i = 0; i < arr.length; i++) {
+
+        const numb = arr[i].toString().split("").map(Number)
+        const deleteMatch = []
+
+        for (let j = i + 1; j < arr.length; j++) {
+
+            const nextNumb = arr[j].toString().split("").map(Number)
+
+            if (findMatch(numb, nextNumb)) {
+                deleteMatch.push(j)
+            }
+        }
+
+        if (deleteMatch.length > 0) {
+
+            deleteMatch.forEach((item, index) => arr.splice(item - index, 1))
+            arr.splice(i, 1)
+            i--
+        }
+
+    }
+    console.log(ARRAY)
+}
+
+function findMatch(numb, nextNumb) {
+
+    const big = numb.length > nextNumb.length ? numb : nextNumb
+    const min = numb === big ? nextNumb : numb
+
+    let match = false
+
+    for (let i = 0; i < big.length; i++) {
+
+        for (let j = 0; j < big.length; j++) {
+
+            if (!match) {
+
+                match = big[i] === min[j] ? i : false
+                if (match) return match
+            }
+
+        }
+    }
+}
+
+function chekToNextStep(prevArrLength, arrLength) {
+
+    if (arrLength < 2) { return true }
+    if (arrLength === prevArrLength) return true
+
+    return false
+}
+
+function reverseAndSquareArr(arr) {
+    console.log('reverse', arr)
+
+    arr.forEach((item, index) => {
+        const buff = +item.toString().split("").map(Number).reverse().join("")
+        arr[index] = Math.pow(buff, 2)
+    })
+}
